0ae17ca8f33ad93ffaa0b97cbd3df79622127f96
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-12-21 12:56:24 +0800

    將store拆成不同檔案管理

---
+++ src/store/counter.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {counter: 0, showCounter: true}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState, //等同initialState:initialState
+    reducers: {
+        // 不須再寫action.type的if check
+        // 使用了toolkit之後seems可以直接mutate existing state
+        // 是因為toolkit會使用內部的imgur package來偵測code並且自動複製existing state，產生新的state obj，讓所有的state都保存下來
+        increment(state) {
+            state.counter++
+        },
+        
+        decrement(state) {
+            state.counter--
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter
+        }
+    }
+})
+
+// counterSlice.actions.toggleCounter() returns an action obj of this shape: { type: 'some auto-generated unique identifier' }
+export const counterActions = counterSlice.actions
+
+// 因為index只需要.reducer
+export default counterSlice.reducer
--- src/store/index.js
+++ src/store/index.js
@@ -1,54 +1,15 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 
-const initialCounterState = {counter: 0, showCounter: true}
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState, //單寫initialState,等同initialState:initialState
-    reducers: {
-        // 不須再寫action.type的if check
-        // 使用了toolkit之後seems可以直接mutate existing state
-        // 是因為toolkit會使用內部的imgur package來偵測code並且自動複製existing state，產生新的state obj，讓所有的state都保存下來
-        increment(state) {
-            state.counter++
-        },
-        
-        decrement(state) {
-            state.counter--
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter
-        }
-    }
-})
-
-const initialAuthState = { isAuth: false }
-const authSlice = createSlice({
-    name: 'auth',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuth = true
-        },
-        logout(state) {
-            state.isAuth = false
-        }
-    }
-})
+import counterReducer from "./counter"
+import authReducer from "./auth"
 
 const store = configureStore({
     // reducer: { counter: counterSlice.reducer }
     reducer: {
-        counter: counterSlice.reducer, 
-        auth: authSlice.reducer
+        counter: counterReducer, 
+        auth: authReducer
     } 
 })
 
-// counterSlice.actions.toggleCounter() returns an action obj of this shape: { type: 'some auto-generated unique identifier' }
-export const counterActions = counterSlice.actions
-export const authActions = authSlice.actions
 
 export default store
